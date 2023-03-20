aa40ce7a2f0a24c58efd67667b1d4c615fcf0a55
EC2 Default User <ec2-user@ip-172-31-55-169.ec2.internal>
2023-03-20 20:47:44 +0000

    tarefa

---
+++ ex10.py
@@ -0,0 +1,14 @@
+ano = int(input("Digite o ano que você nasceu: "))
+
+
+
+
+if ano <= 2005:
+    print("Você poderá votar esse ano")
+
+else:
+    print("Você não pode votar")
+    
+
+if ano <= 1952:
+    print("Você pode votar mas não é obrigatório")
---
+++ ex8.py
@@ -0,0 +1,8 @@
+n = int(input("Números de termos: "))
+a1 = int(input("Valor inicial: "))
+an = int(input("Valor final: "))
+
+
+S = n*(a1+ an)/2.
+
+print("O resultado da soma dos termos da PA é:", S)
